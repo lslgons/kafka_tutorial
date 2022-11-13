592ee4941c9925ae6940b76fca635837a567c73f
daegon.kim <lslgons@naver.com>
2022-11-13 15:23:31 +0900

    Init files

---
+++ src/main/java/com/dizzy/Main.java
@@ -0,0 +1,32 @@
+package com.dizzy;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("====> Start Kafka Producer Tutorial");
+        Properties props=new Properties();
+        props.put("bootstrap.servers", "peter-kafka01.foo.bar:9092,peter-kafka02.foo.bar:9092,peter-kafka03.foo.bar:9092");
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        Producer<String, String> producer=new KafkaProducer<>(props);
+        try {
+            for(int i=0; i<3; ++i) {
+                ProducerRecord<String, String> record=new ProducerRecord<>("peter-basic01",
+                        "Apache Kafka is a distributed streaming platform - "+i);
+                producer.send(record);
+            }
+        } catch(Exception e) {
+            e.printStackTrace();
+        } finally {
+            producer.close();
+        }
+        System.out.println("====> End Kafka Producer Tutorial");
+
+    }
+}
