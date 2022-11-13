9fee4cd3a447401472dbd3fa07b780c220511102
daegon.kim <lslgons@naver.com>
2022-11-13 15:55:16 +0900

    Kafka Producer with ack

--- src/main/java/com/dizzy/Main.java
+++ src/main/java/com/dizzy/Main.java
@@ -9,7 +9,8 @@ import java.util.Properties;
 public class Main {
     public static void main(String[] args) {
         System.out.println("====> Start Kafka Producer Tutorial");
-        KafkaProducerNoACK.main(args);
+        //KafkaProducerNoACK.main(args);
+        KafkaProducerSync.main(args);
 
         System.out.println("====> End Kafka Producer Tutorial");
 
